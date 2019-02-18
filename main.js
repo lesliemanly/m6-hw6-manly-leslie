1930b241f148689923367dc6dbaf259a367cf1e9
Leslie Manly <leslie.manly@ufl.edu>
2019-02-17 19:36:04 -0600

    Add CSS and functions to the button on the page

---
+++ main.js
@@ -0,0 +1,9 @@
+var button = document.getElementsByTagName('button')[0];
+var myHeading = document.getElementsByTagName('h1') [0];
+
+var myfunction = function() {
+  myHeading.classList.add('big-and-bold');
+};
+
+
+button.onclick = myFunction;
