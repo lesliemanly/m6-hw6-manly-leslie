94dffde1e2cd3636e086b32ede0576429edc8725
Leslie Manly <leslie.manly@ufl.edu>
2019-02-17 20:24:36 -0600

    Add lunch item function to javascript

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var button = document.getElementsByTagName('button')[0];
 var myHeading = document.getElementsByTagName('h1') [0];
 
-var myfunction = function() {
-  myHeading.classList.add('big-and-bold');
+var lunchItems = ['Mexican Food', 'Sushi', 'Chicken Wings', 'Pizza', 'Greek Food', 'Subs and Sandwhiches', 'Italian Food', 'Fried Chicken', 'Chicken Fried Steak', 'Chinese Food', 'Breakfast Food', 'BBQ'];
+
+
+var myFunction = function() {
+  lunchItems.sort(function(a, b){return 0.5 - Math.random()});
+  console.log(lunchItems);
 };
 
 
