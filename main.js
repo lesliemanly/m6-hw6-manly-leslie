072061df67970fdd13bb53a2686927f33b395591
Leslie Manly <leslie.manly@ufl.edu>
2019-02-17 21:03:21 -0600

    Add color to the lunch generator

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 var button = document.getElementsByTagName('button')[0];
 var myHeading = document.getElementsByTagName('h1') [0];
 
+var ourRandomLunchItem = document.createElement('p');
+var lunchItemWrapper = document.getElementById('generator-results');
+lunchItemWrapper.appendChild(ourRandomLunchItem);
+
+var placeholderText = document.createTextNode('');
+ourRandomLunchItem.appendChild(placeholderText);
+
 var lunchItems = ['Mexican Food', 'Sushi', 'Chicken Wings', 'Pizza', 'Greek Food', 'Subs and Sandwhiches', 'Italian Food', 'Fried Chicken', 'Chicken Fried Steak', 'Chinese Food', 'Breakfast Food', 'BBQ'];
 
 
 var myFunction = function() {
   lunchItems.sort(function(a, b){return 0.5 - Math.random()});
-  console.log(lunchItems);
+  var ourRandomLunchItemFiller = document.createTextNode(lunchItems[0]);
+  ourRandomLunchItem.appendChild(ourRandomLunchItemFiller);
+  var ourButtonSection = document.getElementById('generator-button');
+  ourButtonSection.removeChild(button);
+  ourRandomLunchItem.classList.add('big-and-bold');
 };
 
 
